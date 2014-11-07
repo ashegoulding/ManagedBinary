d2052ea344cd35625c35bdecd9a7882eb7c5aaf8
Ashe David Goulding <david@danusys.com>
2014-11-07 13:03:28 +0900

    ManagedBinary<T> 싹 다 다시만듦. 이제 size manipulation 잘 작동 함. fill() 메서드 추가. clearOnDelete 멤버 추가: 외부로 양도할 때, 내부 swap() 메서드에서 활용

--- ManagedBinary.h
+++ ManagedBinary.h
@@ -2,6 +2,9 @@
 #define ASHE_MANAGEDBINARY_H_
 
 #include <cstring>
+#include <string>
+#include <sstream>
+#include <typeinfo>
 
 namespace ashe
 {
@@ -14,141 +17,219 @@ public:
 
 	T *data;
 	size_t size, realSize;
+	bool clearUpOnDelete;
 
 protected:
-	void __construct(const thisClass *src) throw()
-	{
-		if(src->data && src->size)
-		{
-			delete[] this->data;
-			this->data = 0;
-			this->realSize = this->size = 0;
+	void __construct(const thisClass *src) throw();
 
-			this->realSize = this->size = src->size;
-			this->data = new T[this->size];
-			memcpy(this->data, src->data, this->size * sizeof(T));
-		}
-		else
-		{
-			memset(this, 0, sizeof(thisClass));
-		}
-	};
+	std::string __dummyString;
 
 public:
-	ManagedBinary(const size_t size = 0) throw()
-		: data(0)
-		, size(0)
-		, realSize(0)
+	ManagedBinary(const size_t size = 0) throw();
+	ManagedBinary(const T *arr, const size_t size) throw();
+	ManagedBinary(const thisClass *src) throw();
+	ManagedBinary(const thisClass &src) throw();
+	virtual ~ManagedBinary() throw();
+
+	virtual thisClass &operator =(const thisClass &src) throw();
+	virtual thisClass *operator =(const thisClass *src) throw();
+
+	virtual void setSize(const size_t size) throw();
+	virtual void resize(const size_t newSize, const bool forced = true) throw();
+	inline virtual void fillZeros() throw();
+	inline virtual void fill(const T x) throw();
+	inline virtual void fill(const unsigned char x) throw();
+	inline virtual void empty() throw();
+	inline virtual void swap(thisClass &with) throw();
+
+	virtual std::string toString(const char **y = NULL) throw();
+};
+
+template<class T>
+ManagedBinary<T>::ManagedBinary(const size_t size) throw ()
+	: data(NULL)
+	, size(0)
+	, realSize(0)
+	, clearUpOnDelete(true)
+{
+	if(size)
+		this->setSize(size);
+}
+
+template<class T>
+ManagedBinary<T>::ManagedBinary(const T* arr, const size_t size) throw ()
+	: data(NULL)
+	, size(0)
+	, realSize(0)
+	, clearUpOnDelete(true)
+{
+	if(arr)
 	{
+		size_t copySize;
 		if(size)
-			this->setSize(size);
+			copySize = size*sizeof(T);
+		else
+			copySize = strlen(arr);
+		this->setSize(copySize);
+		memcpy(this->data, arr, copySize);
 	}
-	ManagedBinary(const thisClass *src) throw()
-		: data(0)
-		, size(0)
-		, realSize(0)
+}
+
+template<class T>
+ManagedBinary<T>::ManagedBinary(const thisClass* src) throw ()
+	: data(NULL)
+	, size(0)
+	, realSize(0)
+	, clearUpOnDelete(true)
+{
+	this->__construct(src);
+}
+
+template<class T>
+ManagedBinary<T>::ManagedBinary(const thisClass& src) throw ()
+	: data(NULL)
+	, size(0)
+	, realSize(0)
+	, clearUpOnDelete(true)
+{
+	this->__construct(&src);
+}
+
+template<class T>
+ManagedBinary<T>::~ManagedBinary() throw ()
+{
+	if(this->clearUpOnDelete)
+		this->empty();
+}
+
+template<class T>
+ManagedBinary<T>& ManagedBinary<T>::operator =(const thisClass& src) throw ()
+{
+	this->__construct(&src);
+	return *this;
+}
+
+template<class T>
+ManagedBinary<T>* ManagedBinary<T>::operator =(const thisClass* src) throw ()
+{
+	this->__construct(src);
+	return this;
+}
+
+template<class T>
+void ManagedBinary<T>::__construct(const thisClass* src) throw ()
+{
+	this->setSize(src->size);
+	if(src->data)
+		memcpy(this->data, src->data, sizeof(T)*this->size);
+	this->clearUpOnDelete = src->clearUpOnDelete;
+}
+
+template<class T>
+void ManagedBinary<T>::setSize(const size_t size) throw ()
+{
+	if(this->realSize == size)
 	{
-		this->__construct(src);
+		// Case: No change
+		this->size = size;
+		this->fillZeros();
+		return;
 	}
-	ManagedBinary(const thisClass &src) throw()
-		: data(0)
-		, size(0)
-		, realSize(0)
+	this->empty();
+	if(size)
 	{
-		this->__construct(&src);
+		this->realSize = this->size = size;
+		this->data = new T[this->realSize];
+		this->fillZeros();
 	}
-	virtual ~ManagedBinary() throw()
+}
+
+template<class T>
+void ManagedBinary<T>::resize(const size_t newSize, const bool forced) throw ()
+{
+	if(forced)
+	{
+		// Case: Alias of thisClass::setSize()
+		this->setSize(newSize);
+		return;
+	}
+	else if(this->realSize >= newSize)
+	{
+		// Case: Not necessary to grow
+		this->size = newSize;
+		return;
+	}
+	// Case: Need to grow
+	// TODO: Grow as requested.
+	thisClass newOne(newSize);
+	if(this->size)
+		memcpy(newOne.data, this->data, sizeof(T)*this->size);
+	this->clearUpOnDelete = true;
+	this->swap(newOne);
+}
+
+template<class T>
+inline void ManagedBinary<T>::fillZeros() throw ()
+{
+	memset(this->data, 0, sizeof(T)*this->size);
+}
+
+template<class T>
+inline void ManagedBinary<T>::empty() throw ()
+{
+	if(this->data || this->realSize)
 	{
 		delete[] this->data;
-		this->data = 0;
+		this->data = NULL;
 		this->realSize = this->size = 0;
 	}
+}
 
-	virtual thisClass &operator =(const thisClass &src) throw()
-	{
-		this->__construct(&src);
-		return *this;
-	}
-	virtual thisClass *operator =(const thisClass *src) throw()
-	{
-		this->__construct(src);
-		return this;
-	}
+template<class T>
+inline void ManagedBinary<T>::swap(thisClass& with) throw ()
+{
+	thisClass dummyOne;
+	memcpy(&dummyOne, &with, sizeof(thisClass));
+	memcpy(&with, this, sizeof(thisClass));
+	memcpy(this, &dummyOne, sizeof(thisClass));
+	dummyOne.clearUpOnDelete = false;
+}
 
-	virtual void setSize(const size_t size) throw()
-	{
-		if(this->size != size)
-		{
-			delete[] this->data;
-			this->data = 0;
-			this->size = this->realSize = size;
-			if(this->realSize)
-				this->data = new T[this->realSize];
-		}
-		memset(this->data, 0, this->realSize * sizeof(T));
-	}
-	virtual void resize(const size_t newSize, const bool forced = true) throw()
-	{
-		if(forced)
-		{
-			if(! newSize)
-			{
-				delete[] this->data;
-				this->data = 0;
-				this->realSize = this->size = 0;
-			}
-			else
-			{
-				T *newData = new T[newSize];
-				memset(newData, 0, newSize  * sizeof(T));
-				memcpy(newData, this->data, newSize * sizeof(T));
-				delete[] this->data;
-				this->data = 0;
-				this->realSize = this->size = 0;
-				this->realSize = this->size = newSize;
-				this->data = newData;
-			}
-		}
-		else
-		{
-			if(this->size < newSize)
-			{
-				T *newData = new T[newSize];
-				memset(newData, 0, newSize  * sizeof(T));
-				memcpy(newData, this->data, newSize * sizeof(T));
-				delete[] this->data;
-				this->data = 0;
-				this->realSize = this->size = 0;
-				this->realSize = this->size = newSize;
-				this->data = newData;
-			}
-			else
-				this->size = size;
-		}
-	}
-	virtual void fillZeros() throw()
-	{
-		memset(this->data, 0, this->size * sizeof(T));
-	}
-	virtual void empty() throw()
-	{
-		delete[] this->data;
-		this->data = 0;
-		this->realSize = this->size = 0;		
-	}
-	virtual void swap(thisClass &with) throw()
-	{
-		T *data = this->data;
-		const size_t size = this->size, realSize = this->realSize;
+template<class T>
+inline void ManagedBinary<T>::fill(const T x) throw ()
+{
+	T *p = this->data;
+	const T *end = p + this->size;
+	while(p < end)
+		*(p++) = x;
+}
 
-		this->data = with.data;
-		this->size = with.size;
-		this->realSize = with.realSize;
-		with.data = data;
-		with.size = size;
-		with.realSize = realSize;
+template<class T>
+inline void ManagedBinary<T>::fill(const unsigned char x) throw ()
+{
+	memset(this->data, x, sizeof(T)*this->size);
+}
+
+template<class T>
+std::string ManagedBinary<T>::toString(const char** y) throw ()
+{
+	std::stringstream sb;
+	sb << "[ManagedBinary<" << typeid(T).name() << "> @ " << std::hex << this << std::dec << "]\n";
+	sb << "size: " << this->size << ", " << this->size*sizeof(T) << (this->size > 1? "bytes" : "byte");
+	if(this->size == this->realSize)
+		sb << "( == realSize)";
+	else
+		sb << "\nrealSize: " << this->realSize << ", " << this->realSize*sizeof(T) << (this->realSize > 1? "bytes" : "byte");
+	if(! this->clearUpOnDelete)
+		sb << "\nWill not clear up on delete.";
+
+	if(y)
+	{
+		this->__dummyString = sb.str();
+		*y = this->__dummyString.c_str();
 	}
-};
+	return sb.str();
+}
 
 } // namespace 'ashe'
 
--- tester.cpp
+++ tester.cpp
@@ -6,13 +6,56 @@
  */
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
 #include "ManagedBinary.h"
 
+typedef ashe::ManagedBinary<unsigned long long int> TestType;
+
+static inline void __report(TestType &x) throw()
+{
+	std::cout << x.toString() << std::endl;
+}
+
+bool threadFlag = false;
+std::mutex mtx;
+std::condition_variable cv;
+void threadRun(TestType *type, unsigned long long *n)
+{
+	std::unique_lock<std::mutex> lck(mtx);
+	while(::threadFlag)
+	{
+		cv.wait_for(lck, std::chrono::seconds(1));
+		if(! ::threadFlag)
+			break;
+		::__report(*type);
+		std::cout << (int)((double)*n / (double)0xFFF * 100.0F) << "% ..." << std::endl;
+	}
+}
+
 int main(const int argc, const char **args)
 {
-	ashe::ManagedBinary<unsigned char> bm;
-	bm.setSize(1024);
+	TestType b;
+	unsigned long long i = 0;
+	srand(time(NULL));
+
+	::threadFlag = true;
+	std::thread th(::threadRun, &b, &i);
+
+	for(i=0; i<0xFFF; ++i)
+		b.resize(abs(rand())%(1024*1024*8), false);
+	for(i=0; i<0xFFF; ++i)
+		b.setSize(abs(rand())%(1024*1024*8));
+
+	::threadFlag = false;
+	cv.notify_all();
+	th.join();
+	::__report(b);
 
 	return 0;
 }
